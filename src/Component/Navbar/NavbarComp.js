f9a3e578a4310462e999c7681f85ffe8133143f4
rifqifinaldy <rifqifinaldy@gmail.com>
2022-01-19 22:28:39 +0700

    add active class on Navbar

--- src/Component/Navbar/NavbarComp.js
+++ src/Component/Navbar/NavbarComp.js
@@ -1,48 +1,85 @@
-import { faBars, faTimes } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import React, { useEffect, useState } from 'react'
-import { Navbar } from 'react-bootstrap'
-import { Link } from 'react-router-dom'
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React, { useEffect, useState } from "react";
+import { Navbar } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+import catalog from "../../asset/img/catalog.pdf";
 
 const NavbarComp = () => {
-    const [show, setShow] = useState(false)
-    const toggle = () => {
-        setShow(!show)
+  const [show, setShow] = useState(false);
+  const toggle = () => {
+    setShow(!show);
+  };
+
+  const navscroll = () => {
+    const navbar = document.getElementById("navbar");
+    if (window.scrollY >= 80) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
     }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", navscroll);
+  });
+  return (
+    <Navbar
+      expand="lg"
+      className={`p-2 ${show === false ? "hide" : "show"}`}
+      id="navbar"
+      onScroll={navscroll}
+    >
+      <div className="logo ms-5">
+        <span>GEOTOUR </span>
+        <span>BATUR</span>
+      </div>
+      <div className="menu-list ms-auto mx-5">
+        <div className="cancel-btn text-right" onClick={toggle}>
+          <FontAwesomeIcon icon={faTimes} />
+        </div>
+        <div className="menu-items">
+          <NavLink exact to="/" className="link" activeClassName="active-link">
+            Home
+          </NavLink>
+          <NavLink
+            exact
+            to="/about"
+            className="link"
+            activeClassName="active-link"
+          >
+            About
+          </NavLink>
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href={catalog}
+            className="link"
+          >
+            Catalog
+          </a>
+          <NavLink
+            exact
+            to="/information-center"
+            className="link"
+            activeClassName="active-link"
+          >
+            Information Center
+          </NavLink>
+          <NavLink
+            exact
+            to="/contact"
+            className="link"
+            activeClassName="active-link"
+          >
+            Contact
+          </NavLink>
+        </div>
+        <div className="menu-open " onClick={toggle}>
+          <FontAwesomeIcon icon={faBars} />
+        </div>
+      </div>
+    </Navbar>
+  );
+};
 
-    const navscroll = () => {
-        const navbar = document.getElementById("navbar")
-        if (window.scrollY >= 80) {
-            navbar.classList.add("sticky");
-        } else { navbar.classList.remove("sticky"); }
-    }
-    useEffect(() => {
-        window.addEventListener('scroll', navscroll)
-    })
-    return (
-
-            <Navbar expand="lg" className={`p-2 ${show === false ? "hide" : "show"}`} id="navbar" onScroll={navscroll}>
-                <div className='logo ms-5'>
-                    <span>GEOTOUR </span>
-                    <span>BATUR</span>
-                </div>
-                <div className='menu-list ms-auto mx-5'>
-                    <div className='cancel-btn text-right' onClick={toggle}>
-                        <FontAwesomeIcon icon={faTimes} />
-                    </div>
-                    <div className="menu-items">
-                        <Link to="/" className='link'>Home</Link>
-                        <Link to='/about' className='link'>About</Link>
-                        <Link to='/#' className='link'>Catalog</Link>
-                        <Link to='/information-center' className='link'>Information Center</Link>
-                        <Link to='/contact' className='link'>Contact</Link>
-                    </div>
-                    <div className='menu-open ' onClick={toggle}>
-                        <FontAwesomeIcon icon={faBars} />
-                    </div>
-                </div>
-            </Navbar>
-    )
-}
-
-export default NavbarComp
+export default NavbarComp;
