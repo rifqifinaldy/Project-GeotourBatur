0517e13214e2a2adf02cb9f527d02629829307c2
rifqifinaldy <rifqifinaldy@gmail.com>
2022-01-19 20:14:57 +0700

    Fix whatsapp message and number

--- src/Component/Whatsapp/Whatsapp.js
+++ src/Component/Whatsapp/Whatsapp.js
@@ -7,7 +7,7 @@ const Whatsapp = () => {
     <div>
       <a
         style={{ color: "white" }}
-        href="https://api.whatsapp.com/send?phone=+6282121988204&text=Hi,%your%20en%20su%20web%20y%20necesito%20consultar%20sobre..."
+        href="https://api.whatsapp.com/send?phone=+6282121988204"
         className="whatsapp-float"
         target="_blank"
         rel="noopener noreferrer"
