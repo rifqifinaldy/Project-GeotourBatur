119f498089baad39efec441dd3f0d67a4b98ce05
nizarmarifat <nizar.marifat@socketspace.com>
2022-01-22 19:29:39 +0700

    add Lighbox, Background img + responsive home page

--- src/Component/OurServices/OurServices.js
+++ src/Component/OurServices/OurServices.js
@@ -4,33 +4,35 @@ import { dataCardServices } from './DataCardServices'
 
 const OurServices = () => {
     return (
-        <Container>
+        <div className='wrapper-services'>
             {/* SERVICES */}
-            <div className='services my-4 text-center'>
+            <div className='services py-3 text-center'>
                 <div className='title'>
                     <h4>Our Services</h4>
                     <h2>Traveling Learning Geoutourism</h2>
                     <hr />
                 </div>
             </div>
-            <Row className='justify-content-center '>
-                {dataCardServices.map((item, i) => {
-                    return (
-                        <Col key={i} xl="3" md="5" sm="12" className='card-services p-2 m-3 shadow '>
-                            <div className='img-services '>
-                                <img src={item.img} />
-                            </div>
-                            <div className='caption-services p-3'>
-                                <h4>{item.title}</h4>
-                                <strong>{item.start} <span className='harga'>{item.harga_pax}</span> {item.min} <span className='harga'>{item.harga_pax_2}</span> {item.min_2}</strong> <br />
-                                <span>{item.desc}</span> <br />
-                                <strong>{item.info}</strong>
-                            </div>
-                        </Col>
-                    )
-                })}
-            </Row>
-        </Container>
+            <Container>
+                <Row className='justify-content-center '>
+                    {dataCardServices.map((item, i) => {
+                        return (
+                            <Col key={i} xl="3 m-3" md="5 m-3" className='card-services p-2 shadow '>
+                                <div className='img-services '>
+                                    <img src={item.img}  />
+                                </div>
+                                <div className='caption-services p-3'>
+                                    <h4>{item.title}</h4>
+                                    <strong>{item.start} <span className='harga'>{item.harga_pax}</span> {item.min} <span className='harga'>{item.harga_pax_2}</span> {item.min_2}</strong> <br />
+                                    <span>{item.desc}</span> <br />
+                                    <strong>{item.info}</strong>
+                                </div>
+                            </Col>
+                        )
+                    })}
+                </Row>
+            </Container>
+        </div>
     )
 }
 
--- src/Component/TriviaGeotour/TriviaGeotour.js
+++ src/Component/TriviaGeotour/TriviaGeotour.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import { Container, NavItem } from 'react-bootstrap'
+import {useState} from 'react'
+import { Button, Col, Container, Row } from 'react-bootstrap'
 import TinySlider from 'tiny-slider-react'
 import { DataTriviaGeotour } from './DataTriviaGeotour'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import img from '../../asset/img/bg-2.png'
+
 const settings = {
     lazyload: true,
     nav: false,
-    mouseDrag: true,
-    mouseDrag: true,
     loop: true,
     responsive: {
         540: {
@@ -18,20 +20,35 @@ const settings = {
     }
 };
 const TriviaGeotour = () => {
+    
+    const [imgShowClass, setImgShowClass] = useState("d-none");
+    const [getImg, setGetImg] = useState();
+
+    
+    const handleShow = () => {
+        setImgShowClass("d-none")
+    }
     return (
-        <div>
-            <div className='services my-4 text-center'>
-                <div className='title'>
+        <div className='wrapper-gallery'>
+            {/* <Row className='justify-content-center align-items-center py-4 text-center'> */}
+                <div className='title text-center py-5'>
                     <h4>Geotour Batur Gallery</h4>
                     <h2>Trivia Geoutour Batur</h2>
                     <hr />
+                </div>
+            <div className={`${imgShowClass}` + " wrapper-img-show"}>
+                <div className='img-card'>
+                    <Button color="danger" size="sm" className="mb-2" onClick={handleShow}>
+                        <FontAwesomeIcon icon={faTimes} />
+                    </Button>
+                    <img src={getImg} />
                 </div>
             </div>
             <Container>
                 <TinySlider settings={settings}>
                     {DataTriviaGeotour.map((item, i) => (
                         <div key={i} className='gallery'>
-                            <img src={item.img} />
+                            <img src={item.img} onClick={()=>{setGetImg(item.img);setImgShowClass("open-img")}} style={{cursor:'pointer'}}/>
                         </div>
                     ))}
                 </TinySlider>
